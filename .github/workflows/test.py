02ec818d2f780e098bafc4b0cf051acd3daf3efd
Nikola Jankovic <nikola.jankovic@eox.at>
2024-06-27 15:34:25 +0200

    updated script

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,12 +1,14 @@
 from github import Auth, Github
+import os
 
-
-token = "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" # get from GITHUB_TOKEN env var
+token = os.environ["GITHUB_TOKEN"]
+repo = os.environ["GITHUB_REPOSITORY"]
+pull_request = int(os.environ["GITHUB_REF_NAME"].split("/")[0])
 
 auth = Auth.Token(token)
 g = Github(auth=auth)
 
-repo = g.get_repo("EOEPCA/open-science-catalog-metadata-testing") # get from GITHUB_REPOSITORY
-pr = repo.get_pull(11) # get from GITHUB_REF_NAME as <pr_number>/merge
+repo = g.get_repo(repo)
+pr = repo.get_pull(pull_request)
 for f in pr.get_files():
-    print(f.filename) # validate each file against appropriate schema
+    print(f.filename)
